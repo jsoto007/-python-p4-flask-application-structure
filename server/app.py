651b461ed8c49ede592ae9afb285d6bad74ed570
jsoto007 <jsoto7087@gmail.com>
2025-03-06 19:25:09 -0500

    working w/ no erorrs

--- server/app.py
+++ server/app.py
@@ -1,5 +1,20 @@
-#!/usr/bin/env python3
+    #!/usr/bin/env python3
 
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+@app.route('/user-age/<int:age>')
+def user_age(age):
+    return f'<h3>I am this {age} many years old</h3>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
